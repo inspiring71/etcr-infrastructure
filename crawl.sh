cfb9b3b8bc1ad4bd03ab7f0b4e715c1564a80716
farshad <fkazemi@dc-dyn-user1-129-97-165-177.dynamic.uwaterloo.ca>
2023-06-21 14:32:17 -0400

    made small changes

--- crawl.sh
+++ crawl.sh
@@ -1,20 +1,23 @@
-userr=
-token=
+userr=rebels
+# token=$token
 logdir=logs
 propfile=$userr.properties
 
-repos=(
-
-)
+# repos=(
+# oracle/graal
+# )
+set -f                      # avoid globbing (expansion of *).
+repos=(${repositories//,/ })
+# repos = ',' read -ra ARRAY <<< "$repositories"
 
 mkdir $logdir/$userr
 for repo in "${repos[@]}"
 do
-    echo -e "spring.main.web-application-type=none\n\n# scraping config" > $propfile
-    echo -e "scraping.repository=$repo" >> $propfile
-    echo -e "scraping.auth=token $token\nscraping.export.all=false\nscraping.export.classified=false" >> $propfile
-    echo -e "# database config\nspring.jpa.hibernate.ddl-auto=update\n" >> $propfile
-    echo -e "spring.datasource.url=jdbc:postgresql://localhost:5432/$repo" >> $propfile
-    echo -e "spring.datasource.username=FAREAST.v-zhuoli1\nspring.datasource.password=passward" >> $propfile
+    echo "spring.main.web-application-type=none\n\n# scraping config" > $propfile
+    echo "scraping.repository=$repo" >> $propfile
+    echo "scraping.auth=token $token\nscraping.export.all=false\nscraping.export.classified=false" >> $propfile
+    echo "# database config\nspring.jpa.hibernate.ddl-auto=update\n" >> $propfile
+    echo "spring.datasource.url=jdbc:postgresql://$db_host:$db_port/$repo" >> $propfile
+    echo "spring.datasource.username=$db_user\nspring.datasource.password=$db_pass" >> $propfile
     java -jar build/libs/prscraper-1.0.1.jar --spring.config.location=$propfile > $logdir/$userr/${repo/\//-}.txt
 done
