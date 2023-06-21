1a557922de225e2682bf0b234df501d08613d5c2
farshad <fkazemi@dc-dyn-user1-129-97-165-177.dynamic.uwaterloo.ca>
2023-06-21 16:05:32 -0400

    fixed vairable name in createdb.sh

--- createdbs.sh
+++ createdbs.sh
@@ -3,13 +3,13 @@
 # repos=(
 # oracle/graal
 # )
-set -f                      # avoid globbing (expansion of *).
-repos=(${repositories//,/ })
+repos = ',' read -ra ARRAY <<< "$repositories"
+
 
 for repo in "${repos[@]}"
 do
     # echo "dropdb $repo."
-    # PGPASSWORD=$db_pass dropdb -h $db_host -p $db_port -U $db_user $repo
+    # PGPASSWORD=$db_pass dropdb -h $db_host -p $db_port -U $db_user -W $repo
     echo "createdb $repo."
-    PGPASSWORD=$db_pass createdb -h $db_host -p $db_port -U db_user $repo
+    PGPASSWORD=$db_pass createdb -h $db_host -p $db_port -U $db_user -W $repo
 done
