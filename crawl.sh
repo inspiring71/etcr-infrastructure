7757a871fec6a1b5f886ff0791604b4a14ccb931
farshad <fkazemi@dc-dyn-user1-129-97-165-177.dynamic.uwaterloo.ca>
2023-06-21 18:36:26 -0400

    Fixed a bug with crawl.sh

--- crawl.sh
+++ crawl.sh
@@ -20,6 +20,7 @@ do
     echo "scraping.auth=token $token\nscraping.export.all=false\nscraping.export.classified=false" >> $propfile
     echo "# database config\nspring.jpa.hibernate.ddl-auto=update\n" >> $propfile
     echo "spring.datasource.url=jdbc:postgresql://$db_host:$db_port/$repo" >> $propfile
-    echo "spring.datasource.username=$db_user\nspring.datasource.password=$db_pass" >> $propfile
+    echo "spring.datasource.username=$db_user">> $propfile
+    echo "spring.datasource.password=$db_pass" >> $propfile
     java -jar build/libs/prscraper-1.0.1.jar --spring.config.location=$propfile #> $logdir/$userr/${repo/\//-}.txt
 done
