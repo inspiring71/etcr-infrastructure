f47c72fa33508349cc1877395f9010d0a5c5ece8
farshad <fkazemi@dc-dyn-user1-129-97-165-177.dynamic.uwaterloo.ca>
2023-06-21 18:28:28 -0400

    Change the logging from file to terminal

--- crawl.sh
+++ crawl.sh
@@ -21,5 +21,5 @@ do
     echo "# database config\nspring.jpa.hibernate.ddl-auto=update\n" >> $propfile
     echo "spring.datasource.url=jdbc:postgresql://$db_host:$db_port/$repo" >> $propfile
     echo "spring.datasource.username=$db_user\nspring.datasource.password=$db_pass" >> $propfile
-    java -jar build/libs/prscraper-1.0.1.jar --spring.config.location=$propfile > $logdir/$userr/${repo/\//-}.txt
+    java -jar build/libs/prscraper-1.0.1.jar --spring.config.location=$propfile #> $logdir/$userr/${repo/\//-}.txt
 done
